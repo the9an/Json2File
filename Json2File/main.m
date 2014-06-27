97bcc8392b591ce938a861d28b4ae79314eaaa73
quan-nguyen <quan-nguyen@kayac.com>
2014-06-28 04:11:50 +0900

    remove copyright

--- Json2File/main.m
+++ Json2File/main.m
@@ -3,7 +3,7 @@
 //  Json2File
 //
 //  Created by NguyenTheQuan on 2014/05/14.
-//  Copyright (c) 2014年 kayac. All rights reserved.
+//  Copyright (c) 2014年. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
